1b9811d2b9b554521f994b057c2db97971db5341
Eickichi <Davidye75019@gmail.com>
2019-10-11 00:56:17 +0200

    try exo2

--- lib/Mairie_Christmas.rb
+++ lib/Mairie_Christmas.rb
@@ -10,7 +10,7 @@ def get_townhall_urls
     return townhall_urls_array
   end
 
-print get_townhall_urls
+get_townhall_urls
 
 def get_townhall_names
   @page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
@@ -21,8 +21,31 @@ def get_townhall_names
     return townhall_names_array
 end
 
-print get_townhall_names
+get_townhall_names
 
+def get_townhall_email
+    n = get_townhall_urls.count
+    i = 0
+    emails = []
+    while i < n
+        @page = Nokogiri::HTML(open("http://annuaire-des-mairies.com#{get_townhall_urls[i].to_s}"))
+        result = @page.xpath('//section[2]/div/table/tbody/tr[4]/td[2]').map do |node|
+            emails.push(node.text)
+        end
+        p emails[i]
+        i += 1
+    end
+
+    names = get_townhall_names
+    result_email = names.map.with_index do |name, index|
+        new_hash = {}
+        new_hash[name] = emails[index]
+        new_hash
+    end
+    return result_email
+end
+
+print get_townhall_email
 =begin
 
 def get_townhall_email
