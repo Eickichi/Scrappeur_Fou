8d9820358b5365671517bcd52f3b73a40f3f05fc
Eickichi <Davidye75019@gmail.com>
2019-10-10 22:12:36 +0200

    Spec modif

--- spec/Scrappeur_Fou_spec.rb
+++ spec/Scrappeur_Fou_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../lib/Scrappeur_Fou'
 
+describe "for crypto_symbols" do
+    it "should return crypto_symbols" do
+      expect(sign).to be_an_instance_of(Array)
+    end
+end
+
 describe "for crypto_price" do
     it "should return crypto_price array" do
-      expect(sign).to be_an_instance_of(Array)
+      expect(price).to be_an_instance_of(Array)
     end
 end
